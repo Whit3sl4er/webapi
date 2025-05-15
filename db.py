4115bf8185520daf82c1f21af9ee2980041e7042
qeqoq <sanyaksgo@yandex.ru>
2025-05-16 01:20:42 +0300

    Перенос подключения БД на PostgreSQL для Render

--- db.py
+++ db.py
@@ -1,17 +1,7 @@
-import pyodbc
 import os
-from dotenv import load_dotenv
+import psycopg2
 
-load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
-    server = os.getenv("DB_SERVER")
-    database = os.getenv("DB_NAME")
-
-    conn_str = (
-        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
-        f"SERVER={server};"
-        f"DATABASE={database};"
-        f"Trusted_Connection=yes;"
-    )
-    return pyodbc.connect(conn_str)
+    return psycopg2.connect(DATABASE_URL)
