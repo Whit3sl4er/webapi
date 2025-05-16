ba9aa9b8cfecd084d442f1d9e2b706d6c84854d1
qeqoq <sanyaksgo@yandex.ru>
2025-05-16 23:28:56 +0300

    fix split functions

--- routes.py
+++ routes.py
@@ -8,30 +8,30 @@ router = APIRouter()
 
 @router.get("/Users")
 async def get_users():
-    await get_tables.get_users()
+    return await get_tables.get_users()
 
 @router.get("/Subscriptions")
 async def get_subscriptions():
-    await get_tables.get_subscriptions()
+    return await get_tables.get_subscriptions()
 
 @router.get("/LicenseKeys")
 async def get_license_keys():
-    await get_tables.get_license_keys()
+    return await get_tables.get_license_keys()
 
 @router.post("/create-key")
 async def create_license_key(request: models.KeyCreateRequest):
-    await key_logic.create_license_key(request)
+    return await key_logic.create_license_key(request)
 
 @router.post("/activate-key")
 async def activate_license_key(request: models.KeyActivateRequest):
-    await key_logic.activate_license_key(request)
+    return await key_logic.activate_license_key(request)
 
 # Функция регистрации
 @router.post("/register")
 async def register_user(data: models.RegisterData):
-    await authentication.register_user(data)
+    return await authentication.register_user(data)
 
 # Функция авторизации
 @router.post("/login")
 async def login_user(data: models.LoginData):
-    await authentication.logIn_user(data)
+    return await authentication.logIn_user(data)
